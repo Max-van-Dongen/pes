f31f38ead76b5b82539bcbb0c8c7bd16f70f3602
Aron <aron@dingweb.nl>
2024-05-10 16:54:00 +0200

    Big refactor of wemos

--- WeMos/Communicatie + lampjes/WeMos/WeMos.ino
+++ WeMos/Communicatie + lampjes/WeMos/WeMos.ino
@@ -2,7 +2,8 @@
 #include <WiFiClient.h>
 const char* ssid = "coldspot";
 const char* password = "123456781";
-const char* serverIP = "192.168.230.130";
+const char* serverIP = "192.168.25.207";
+const char clientId = 'a';
 
 const int serverPort = 6789;
 
@@ -10,7 +11,7 @@ WiFiClient client;
 
 const int ledje = 0;
 const int ledje2 = 2;
-int i = 0;
+int i = 4;
 int ontvangst = 0;
 
 void setup() {
@@ -31,47 +32,35 @@ void setup() {
 }
 
 void loop() {
-  if (!client.connected()) 
-    if (client.connect(serverIP, serverPort)) {
-      while (true) {
-      Serial.println("Verbonden met server");
-      //if (knopje == HIGH) {
-      client.println("1");
-      delay(100);
-      client.println("Dit is een test \n\n");
-      delay(100);
-      digitalWrite(ledje, LOW);
-      digitalWrite(ledje2, LOW);
-      client.println(i);
-      delay(2500);
-      i++;
-      //if (client.read() != -1) {
-        ontvangst = client.read();
-      while (ontvangst != -1) {
-          Serial.println(ontvangst);
-        if (ontvangst == 49) {
-          digitalWrite(ledje, HIGH);
-          ontvangst = client.read();
-      }
-        if (ontvangst == 50) {
-          Serial.println("Ledje hoor tnu aan te zijn!");
-          digitalWrite(ledje2, HIGH);
-          ontvangst = client.read();
-        }
-      }
-      //}
-      //}
-      if (!client.connected()) {
-        break;
-      }
-      }
-    } else {
-      Serial.println("Verbinding mislukt. Opnieuw proberen...");
-      delay(5000);
-      return;
+  if (!client.connect(serverIP, serverPort)) { 
+    Serial.println("Verbinding mislukt. Opnieuw proberen...");
+    delay(5000);
+    return;
+  }
+    Serial.println("Verbonden met server");
+    Serial.printf("status: %i\r\n", client.status());
+    
+    sendmsg('a','v');
+    
+    delay(100);
+    digitalWrite(ledje, LOW);
+    digitalWrite(ledje2, LOW);
+    
+    delay(2500);
+    //if (client.read() != -1) {
+      ontvangst = client.read();
+    while (ontvangst != -1) {
+      Serial.printf("%c",ontvangst);
+
+
+      ontvangst = client.read();
     }
+    Serial.println();
+}
+
+void sendmsg(char dest, char data) {
+  char buff[50];
   
-  // Send a request every 30 seconds
-  //Serial.println("30 seconden voorbij");
-  //delay(30000);
+  sprintf(buff, "%c%c%c", clientId, dest, data);
+  client.write(buff);
 }
