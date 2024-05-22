6f08e06deefdf7200a34e94acfeba4ba7452248e
Max-van-Dongen <radiomixonly@gmail.com>
2024-05-22 10:10:56 +0200

    Update matrix.ino

--- matrix/matrix.ino
+++ matrix/matrix.ino
@@ -18,10 +18,17 @@ MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
 const char* ssid = "coldspot";
 const char* password = "123456781";
-const char* host = "192.168.54.130";
+const char* host = "192.168.207.130";
 const uint16_t port = 16789;
 
 AsyncClient* client = nullptr;
+
+
+float temperature = 0.0;
+float humidity = 0.0;
+unsigned long previousMillis = 0;
+const long interval = 5000;  // 2 seconds interval
+bool showTemp = true;
 
 void setup() {
   Serial.begin(115200);
@@ -64,12 +71,15 @@ void setup() {
   client->onData([](void* arg, AsyncClient* c, void* data, size_t len) {
     Serial.print("Received: ");
     Serial.write((uint8_t*)data, len);
-        String receivedData = String((char*)data).substring(0, len);
-        float temperature = receivedData.toFloat();
-        displayTemp(temperature);
-    Serial.println("");
+    String receivedData = String((char*)data).substring(0, len);
+    processReceivedString(receivedData);
+    // float temperature = receivedData.toFloat();
+    // displayTemp(temperature);
+    // Serial.println("");
   },
                  nullptr);
+
+
   // Connect to the server
   client->connect(host, port);
   // Intialize the object
@@ -109,20 +119,65 @@ void displayFire(int state) {
     delay(750);
   }
 }
+
+
+void processReceivedString(String input) {
+  int tempIndex = input.indexOf("Temp:");
+  int humidIndex = input.indexOf("Humid:");
+
+  if (tempIndex != -1) {
+    int startIndex = tempIndex + 5;  // Move past "Temp:"
+    int endIndex = (humidIndex != -1 && humidIndex > startIndex) ? humidIndex : input.indexOf(' ', startIndex);
+    if (endIndex == -1) endIndex = input.length();  // If no space found and no Humid:, use the end of the string
+    String tempValueString = input.substring(startIndex, endIndex);
+    temperature = tempValueString.toFloat();
+  }
+
+  if (humidIndex != -1) {
+    int startIndex = humidIndex + 6;  // Move past "Humid:"
+    int endIndex = input.indexOf(' ', startIndex);
+    if (endIndex == -1) endIndex = input.length();  // If no space found, use the end of the string
+    String humidValueString = input.substring(startIndex, endIndex);
+    humidity = humidValueString.toFloat();
+  }
+}
+
 void displayTemp(float temperature) {
   char tempDisplay[10];
   sprintf(tempDisplay, "%2.1f%cC", temperature, '\xB0');
   myDisplay.setTextAlignment(PA_CENTER);
   myDisplay.print(tempDisplay);
 }
-void loop() {
-  if (client && client->connected()) {
 
+void loop() {
+
+  unsigned long currentMillis = millis();
+  if (client && client->connected()) {
   } else {
     Serial.println("Client not connected");
     if (!client->connecting()) {
       client->connect(host, port);
     }
     delay(500);
+  }
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    if (showTemp) {
+      Serial.print("Temperature: ");
+      Serial.println(temperature);
+      char tempDisplay[10];
+      sprintf(tempDisplay, "%2.1f%cC", temperature, '\xB0');
+      myDisplay.setTextAlignment(PA_CENTER);
+      myDisplay.print(tempDisplay);
+    } else {
+      Serial.print("Humidity: ");
+      Serial.println(humidity);
+      char tempDisplay[10];
+      sprintf(tempDisplay, "%.1f%%", humidity);
+      myDisplay.setTextAlignment(PA_CENTER);
+      myDisplay.print(tempDisplay);
+    }
+    showTemp = !showTemp;  // Toggle between temperature and humidity
   }
 }
